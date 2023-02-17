71918efdc85648a4f8539fb5705205480c44f7f2
WilliansonDantas <williansondantas@gmail.com>
2023-02-17 19:54:59 -0300

    feat:implementa função study_schedule,req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,8 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    for left, right in permanence_period:
+        if not isinstance(right, int) or not isinstance(left, int) or not isinstance(target_time, int):
+            return None
+        if left <= target_time <= right:
+            count += 1
+    return count
