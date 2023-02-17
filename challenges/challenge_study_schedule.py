d25b6dd8f5562f93d27b8a1e6f6539c60339e561
WilliansonDantas <williansondantas@gmail.com>
2023-02-17 20:14:27 -0300

    fix:corrige identação req01 com uso do black

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,11 @@
 def study_schedule(permanence_period, target_time):
     count = 0
     for left, right in permanence_period:
-        if not isinstance(
-            right, int) or not isinstance(
-            left, int) or not isinstance(
-            target_time, int):
+        if (
+            not isinstance(right, int)
+            or not isinstance(left, int)
+            or not isinstance(target_time, int)
+        ):
             return None
         if left <= target_time <= right:
             count += 1
