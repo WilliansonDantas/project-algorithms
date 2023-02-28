e9c292688f11dcb4d3feefbbd48770ecafc3c6ab
WilliansonDantas <williansondantas@gmail.com>
2023-02-28 16:22:22 -0300

    feat:implementa testes da função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    testImpar = encrypt_message("abacad", 3)
+    assert testImpar == "aba_dac"
+
+    testPar = encrypt_message("abcdef", 4)
+    assert testPar == "fe_dcba"
+
+    testValueInvalid = encrypt_message("abcdef", 9)
+    assert testValueInvalid == "fedcba"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("abcdef", "3")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 3)
