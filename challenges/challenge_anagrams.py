d2d013fe8fa60a7facee8e6813f91622a3edb49b
WilliansonDantas <williansondantas@gmail.com>
2023-03-02 17:41:35 -0300

    fix:corrige erros e lint da função is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,13 +1,6 @@
-def is_anagram(first_string, second_string):
-    if not isinstance(first_string, str) or not isinstance(second_string, str):
-        return False
-    
-    str1 = first_string.lower()
-    str2 = second_string.lower()
-
-    # Referência Merge Sort: https://github.com/tryber/sd-021-b-live-lectures/pull/104/files#diff-32efdbb5e489278def2e2f091a67da0e920308253823c0197923c41e485d5915
-    # Bloco de código visto na aula do dia 4.3, conforme referência
-    def merge_sort(input_list: list):
+# Referência Merge Sort: https://github.com/tryber/sd-021-b-live-lectures/pull/104/files#diff-32efdbb5e489278def2e2f091a67da0e920308253823c0197923c41e485d5915
+# Bloco de código visto na aula do dia 4.3, conforme referência acima.
+def merge_sort(input_list: list):
         if len(input_list) <= 1:
             return input_list
 
@@ -17,8 +10,9 @@ def is_anagram(first_string, second_string):
         right = merge_sort(input_list[mid:])
 
         return merge(left, right)
-    
-    def merge(left: list, right: list):
+
+
+def merge(left: list, right: list):
         result = []
         while len(left) > 0 and len(right) > 0:
             if left[0] < right[0]:
@@ -32,7 +26,15 @@ def is_anagram(first_string, second_string):
             result += left
 
         return result
-    # Bloco de código visto no repositório da turma, na aula do dia 4.3
+# Bloco de código visto na aula do dia 4.3, conforme referência acima.
+
+
+def is_anagram(first_string, second_string):
+    if not isinstance(first_string, str) or not isinstance(second_string, str):
+        return False
+    
+    str1 = first_string.lower()
+    str2 = second_string.lower()
     
     str1_sorted = merge_sort(str1)
     str2_sorted = merge_sort(str2)
