6456266c53e6e4f9e9dd026220958eab7f2ee56f
WilliansonDantas <williansondantas@gmail.com>
2023-03-02 18:10:53 -0300

    fix:correção de erros de lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,4 @@
-# Referência Merge Sort: 
+# Referência Merge Sort:
 # https://github.com/tryber/sd-021-b-live-lectures/pull/104/
 # Bloco de código visto na aula do dia 4.3, conforme referência acima.
 def merge_sort(input_list: list):
@@ -27,19 +27,24 @@ def merge(left: list, right: list):
         result += left
 
     return result
+
+
 # Bloco de código visto na aula do dia 4.3, conforme referência acima.
 
 
 def is_anagram(first_string, second_string):
     if not isinstance(first_string, str) or not isinstance(second_string, str):
         return False
-    
+
     str1 = first_string.lower()
     str2 = second_string.lower()
-    
+
     str1_sorted = merge_sort(list(str1))
     str2_sorted = merge_sort(list(str2))
 
-    if str1_sorted != str2_sorted:
+    str1Join = "".join(str1_sorted)
+    str2Join = "".join(str2_sorted)
+
+    if str1Join != str2Join:
         return False
-    return "".join(str2_sorted),"".join(str1_sorted), True
+    return str1Join, str2Join, True
