1d3286da2d6fdfabe67749d74d2575bb00204992
WilliansonDantas <williansondantas@gmail.com>
2023-03-03 10:57:13 -0300

    fix:na função find_duplicate, passa a utilizar o método set para testar se dessa forma o github processa o código, já que na resolução anterior ele quebra após muito tempo de processamento, msm o código estando correto

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,11 +1,13 @@
 def find_duplicate(nums):
     if len(nums) < 2:
         return False
-    numberDuplicate = []
+    # numberDuplicate = []
+    numberDuplicate = set()
     for number in nums:
         if not isinstance(number, int) or number < 0:
             return False
         if number in numberDuplicate:
             return number
-        numberDuplicate.append(number)
+        # numberDuplicate.append(number)
+        numberDuplicate.add(number)
     return False
