223ae7d8a0ae0c6b51be50a281be509cee47d857
WilliansonDantas <williansondantas@gmail.com>
2023-03-02 21:30:13 -0300

    fix:correção de lint retirando espaços em branco

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -23,7 +23,7 @@ def find_duplicate(nums):
         if not isinstance(number, int) or number < 0:
             return False
     nums.sort()
-    for index in range(len(nums)-1):
+    for index in range(len(nums) - 1):
         if nums[index] == nums[index + 1]:
             return nums[index]
     return False
