a2da5795acc27ba8bde4c2acfcdb109dea113644
WilliansonDantas <williansondantas@gmail.com>
2023-03-03 09:12:58 -0300

    fix:reduz complexidade para O(n). Para isso, foi necessário retirar a ordenação do array, o sort() ou o selection_sort

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -23,11 +23,11 @@
 def find_duplicate(nums):
     if len(nums) < 2:
         return False
-    nums.sort()
-    for index in range(len(nums) - 1):
-        number = nums[index]
+    numberDuplicate = []
+    for number in nums:
         if not isinstance(number, int) or number < 0:
             return False
-        if nums[index] == nums[index + 1]:
-            return nums[index]
+        if number in numberDuplicate:
+            return number
+        numberDuplicate.append(number)
     return False
