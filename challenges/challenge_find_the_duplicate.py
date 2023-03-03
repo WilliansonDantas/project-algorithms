857f374b9be9722d9a70727cdd8ffcae9759d3bb
WilliansonDantas <williansondantas@gmail.com>
2023-03-02 21:51:03 -0300

    fix:alteração apenas p o avaliador reprocessar

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -5,15 +5,19 @@
 # def selection_sort(input_list: list):
 #     for i in range(len(input_list)):
 #         min_i = i
-
 #         for j in range(i + 1, len(input_list)):
 #             if input_list[j] < input_list[min_i]:
 #                 min_i = j
-
 #         input_list[min_i], input_list[i] = input_list[i], input_list[min_i]
-
 #     return input_list
 # Bloco de código visto na aula do dia 4.3, conforme referência acima.
+
+# Outra forma de fazer utilizando o método selection_sort
+# numberSorted = selection_sort(nums)
+# for index in range(len(numberSorted)-1):
+#     if numberSorted[index] == numberSorted[index + 1]:
+#         return numberSorted[index]
+# return False
 
 
 def find_duplicate(nums):
@@ -27,9 +31,3 @@ def find_duplicate(nums):
         if nums[index] == nums[index + 1]:
             return nums[index]
     return False
-    # Outra forma de fazer utilizando o método selection_sort
-    # numberSorted = selection_sort(nums)
-    # for index in range(len(numberSorted)-1):
-    #     if numberSorted[index] == numberSorted[index + 1]:
-    #         return numberSorted[index]
-    # return False
