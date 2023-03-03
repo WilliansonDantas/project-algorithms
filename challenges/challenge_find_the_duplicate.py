370ebc781e21d6ab3049807386270f6d119ce1ea
WilliansonDantas <williansondantas@gmail.com>
2023-03-03 09:14:45 -0300

    fix:retira trecho de código comentado com solução utilizando selection_sort

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,25 +1,3 @@
-# Outra forma de fazer utilizando o método selection_sort
-# Referência Selection Sort:
-# https://github.com/tryber/sd-021-b-live-lectures/pull/104/
-# Bloco de código visto na aula do dia 4.3, conforme referência acima.
-# def selection_sort(input_list: list):
-#     for i in range(len(input_list)):
-#         min_i = i
-#         for j in range(i + 1, len(input_list)):
-#             if input_list[j] < input_list[min_i]:
-#                 min_i = j
-#         input_list[min_i], input_list[i] = input_list[i], input_list[min_i]
-#     return input_list
-# Bloco de código visto na aula do dia 4.3, conforme referência acima.
-
-# Outra forma de fazer utilizando o método selection_sort
-# numberSorted = selection_sort(nums)
-# for index in range(len(numberSorted)-1):
-#     if numberSorted[index] == numberSorted[index + 1]:
-#         return numberSorted[index]
-# return False
-
-
 def find_duplicate(nums):
     if len(nums) < 2:
         return False
